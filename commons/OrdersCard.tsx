cf501474d1f70379c5729989f425d4383850da78
vishnu <vishnu@neopay.club>
2022-06-01 14:09:56 +0530

    salesman_name name add in order card and Distributor name in order details

--- commons/OrdersCard.tsx
+++ commons/OrdersCard.tsx
@@ -1,119 +1,154 @@
-import * as React from 'react';
-import {View, StyleSheet, Text, TouchableOpacity} from "react-native";
+import * as React from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import colors from "../assets/colors/colors";
 import commonStyles from "../styles/commonStyles";
-import {BorderButtonSmallBlue} from "../buttons/Buttons";
+import { BorderButtonSmallBlue } from "../buttons/Buttons";
 import texts from "../styles/texts";
 import moment from "moment";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from "react-native-vector-icons/FontAwesome";
 import * as Linking from "expo-linking";
 
+const buttonStyle = {
+  ordered: { backgroundColor: colors.primaryThemeColor },
+  released: { backgroundColor: colors.green },
+  approved: { backgroundColor: colors.green },
+  delivered: { backgroundColor: colors.green },
+  rejected: { backgroundColor: colors.red },
+  returned: { backgroundColor: colors.red },
+  cancelled: { backgroundColor: colors.red },
+};
 
 function OrdersCard(props: any) {
-    let itemString = "";
+  let itemString = "";
 
-    const buttonStyle = {
-        "ordered": {backgroundColor: colors.primaryThemeColor},
-        "released": {backgroundColor: colors.green},
-        "approved": {backgroundColor: colors.green},
-        "delivered": {backgroundColor: colors.green},
-        "rejected": {backgroundColor: colors.red},
-        "returned": {backgroundColor: colors.red},
-        "cancelled": {backgroundColor: colors.red},
+  props.data.product_list.map((item) => {
+    itemString += item.name + " x " + item.value + " Units" + ", ";
+  });
 
-    }
+  const viewOrderDetails = () => {
+    props.goToOrderDetails(props.data, props.index);
+  };
 
-    props.data.product_list.map((item) => {
-        itemString += item.name + ' x ' + item.value + ' Units' + ", ";
-    })
+  const callRetailer = (mobile) => {
+    Linking.openURL(`tel:${mobile}`);
+  };
 
-    const viewOrderDetails = () => {
-        props.goToOrderDetails(props.data, props.index)
-    }
-
-    const callRetailer = (mobile) => {
-        Linking.openURL(`tel:${mobile}`)
-    }
-
-    return (
-        <TouchableOpacity onPress={viewOrderDetails} style={commonStyles.ordersCard}>
-            <View style={commonStyles.rowSpaceBetween}>
-                <View style={[commonStyles.rowAlignCenter, {paddingVertical: 6}]}>
-                    <Text style={texts.darkGreyTextBold12}>Order Id: </Text>
-                    <Text style={texts.primaryTextBold12}>{props.data.id}</Text>
-                </View>
-                <View>
-                    <Text style={texts.greyNormal12}>
-                        {moment(props.data.created_at).format("MMMM D, hh:mm A")}
-                    </Text>
-                </View>
-            </View>
-            <View style={commonStyles.rowSpaceBetween}>
-                <View style={[commonStyles.rowAlignCenter, {paddingVertical: 6}]}>
-                    <Text style={texts.darkGreyTextBold12}>Order value: </Text>
-                    <Text style={texts.primaryTextBold12}>{parseFloat(props.data.revised_value).toFixed(2)}</Text>
-                </View>
-                <View style={[commonStyles.rowAlignCenter, commonStyles.statusButton, buttonStyle[props.data.status]]}>
-                    <Text style={[texts.whiteTextBold12, {textTransform: "capitalize"}]}>
-                        {props.data.status}
-                    </Text>
-                </View>
-            </View>
-            <View style={commonStyles.rowSpaceBetween}>
-                <View style={[commonStyles.rowAlignCenter, {paddingVertical: 6}]}>
-                    <Text style={texts.darkGreyTextBold12}>{props.data.distributor_name}</Text>
-                </View>
-                {props.data.created_by !== "" ? <View style={[commonStyles.rowAlignCenter, {paddingVertical: 6}]}>
-                    <Text style={texts.darkGreyTextBold12}>Created by: </Text>
-                    <Text style={texts.primaryTextBold12}>{
-                        props.data.created_by === "retailer" ?
-                            "Self" : props.data.created_by === "salesman" ?
-                                  "Salesman" : "Partner"}</Text>
-                </View> : null}
-            </View>
-            <View style={{flexDirection: "row", flexWrap: "wrap"}}>
-                <Text style={texts.darkGreyTextBold12}>Items: </Text>
-                <Text style={texts.greyNormal12} textBreakStrategy="simple">
-                    {itemString}
-                </Text>
-            </View>
-            <View style={[commonStyles.rowSpaceBetween, {paddingTop: 10}]}>
-                <View style={commonStyles.row}>
-                    <TouchableOpacity onPress={() => {
-                        callRetailer(props.data.distributor_contact)
-                    }} style={[commonStyles.borderButtonSmallBlue, {flexDirection: 'row', alignItems:"center"}]}>
-                        <Icon name="phone" size={14} color={colors.primaryThemeColor}/>
-                        <Text style={texts.primaryTextBold12}> Supplier</Text>
-                    </TouchableOpacity>
-                    {props.data.salesman ? <TouchableOpacity onPress={() => {
-                        callRetailer(props.data.salesman_contact)
-                    }} style={[commonStyles.borderButtonSmallBlue, {flexDirection: "row", marginLeft: 10}]}>
-                        <Icon name="phone" size={14} color={colors.primaryThemeColor}/>
-                        <Text style={texts.primaryTextBold12}> Salesman</Text>
-                    </TouchableOpacity> : null}
-                </View>
-                <View>
-                    <BorderButtonSmallBlue ctaFunction={viewOrderDetails} text={"View Details"}/>
-                </View>
-            </View>
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity
+      onPress={viewOrderDetails}
+      style={commonStyles.ordersCard}
+    >
+      <View style={commonStyles.rowSpaceBetween}>
+        <View style={[commonStyles.rowAlignCenter, { paddingVertical: 6 }]}>
+          <Text style={texts.darkGreyTextBold12}>Order Id: </Text>
+          <Text style={texts.primaryTextBold12}>{props.data.id}</Text>
+        </View>
+        <View>
+          <Text style={texts.greyNormal12}>
+            {moment(props.data.created_at).format("MMMM D, hh:mm A")}
+          </Text>
+        </View>
+      </View>
+      <View style={commonStyles.rowSpaceBetween}>
+        <View style={[commonStyles.rowAlignCenter, { paddingVertical: 6 }]}>
+          <Text style={texts.darkGreyTextBold12}>Order value: </Text>
+          <Text style={texts.primaryTextBold12}>
+            {parseFloat(props.data.revised_value).toFixed(2)}
+          </Text>
+        </View>
+        <View
+          style={[
+            commonStyles.rowAlignCenter,
+            commonStyles.statusButton,
+            buttonStyle[props.data.status],
+          ]}
+        >
+          <Text
+            style={[texts.whiteTextBold12, { textTransform: "capitalize" }]}
+          >
+            {props.data.status}
+          </Text>
+        </View>
+      </View>
+      <View style={commonStyles.rowSpaceBetween}>
+        <View style={[commonStyles.rowAlignCenter, { paddingVertical: 6 }]}>
+          <Text style={texts.darkGreyTextBold12}>
+            {props.data.distributor_name}
+          </Text>
+        </View>
+        {props.data.created_by !== "" ? (
+          <View style={[commonStyles.rowAlignCenter, { paddingVertical: 6 }]}>
+            <Text style={texts.darkGreyTextBold12}>Created by: </Text>
+            <Text style={texts.primaryTextBold12}>
+              {props.data.created_by === "retailer"
+                ? "Self"
+                : props.data.created_by === "salesman"
+                ? props.data.salesman_name
+                : "Partner"}
+            </Text>
+          </View>
+        ) : null}
+      </View>
+      <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+        <Text style={texts.darkGreyTextBold12}>Items: </Text>
+        <Text style={texts.greyNormal12} textBreakStrategy="simple">
+          {itemString}
+        </Text>
+      </View>
+      <View style={[commonStyles.rowSpaceBetween, { paddingTop: 10 }]}>
+        <View style={commonStyles.row}>
+          <TouchableOpacity
+            onPress={() => {
+              callRetailer(props.data.distributor_contact);
+            }}
+            style={[
+              commonStyles.borderButtonSmallBlue,
+              { flexDirection: "row", alignItems: "center" },
+            ]}
+          >
+            <Icon name="phone" size={14} color={colors.primaryThemeColor} />
+            <Text style={texts.primaryTextBold12}> Supplier</Text>
+          </TouchableOpacity>
+          {props.data.salesman ? (
+            <TouchableOpacity
+              onPress={() => {
+                callRetailer(props.data.salesman_contact);
+              }}
+              style={[
+                commonStyles.borderButtonSmallBlue,
+                { flexDirection: "row", marginLeft: 10 },
+              ]}
+            >
+              <Icon name="phone" size={14} color={colors.primaryThemeColor} />
+              <Text style={texts.primaryTextBold12}> Salesman</Text>
+            </TouchableOpacity>
+          ) : null}
+        </View>
+        <View>
+          <BorderButtonSmallBlue
+            ctaFunction={viewOrderDetails}
+            text={"View Details"}
+          />
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    buttonContainer: {
-        flexDirection: 'column',
-        justifyContent: "center",
-    },
-    callIcon: {
-        borderWidth: 1,
-        height: 30,
-        borderColor: colors.primaryThemeColor,
-        borderRadius: 5,
-        alignItems: 'center',
-        flexDirection: 'row',
-        paddingHorizontal: 8
-    },
-})
+  buttonContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  callIcon: {
+    borderWidth: 1,
+    height: 30,
+    borderColor: colors.primaryThemeColor,
+    borderRadius: 5,
+    alignItems: "center",
+    flexDirection: "row",
+    paddingHorizontal: 8,
+  },
+});
 
 export default OrdersCard;
--- screens/orderList/OrderListDetails.tsx
+++ screens/orderList/OrderListDetails.tsx
@@ -1,225 +1,234 @@
-import React, {useEffect, useState} from 'react';
-import {
-    Text,
-    View,
-    StyleSheet,
-    ScrollView,
-    Alert
-} from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Text, View, StyleSheet, ScrollView, Alert } from "react-native";
 import colors from "../../assets/colors/colors";
 import texts from "../../styles/texts";
 import commonStyles from "../../styles/commonStyles";
 import SecondaryHeader from "../../headers/SecondaryHeader";
-import {useFocusEffect, useRoute} from "@react-navigation/native";
-import Icon from 'react-native-vector-icons/Feather';
-import {BorderButtonSmallBlue} from "../../buttons/Buttons";
-import * as WebBrowser from 'expo-web-browser';
-import {commonApi} from "../../api/api";
-import {AuthenticatedGetRequest} from "../../api/authenticatedGetRequest";
+import { useFocusEffect, useRoute } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/Feather";
+import { BorderButtonSmallBlue } from "../../buttons/Buttons";
+import * as WebBrowser from "expo-web-browser";
+import { commonApi } from "../../api/api";
+import { AuthenticatedGetRequest } from "../../api/authenticatedGetRequest";
 
 export default function OrderListDetails() {
+  const route = useRoute();
+  const [orderDetails, setOrderDetails] = useState(
+    route.params.orderDetailsData
+  );
+  const [retailerDetails, setRetailerDetails] = useState({});
 
-    const route = useRoute();
-    const [orderDetails, setOrderDetails] = useState(route.params.orderDetailsData);
-    const [retailerDetails, setRetailerDetails] = useState({});
+  const setOrderData = (data) => {
+    setOrderDetails(data);
+  };
 
-
-    const setOrderData = (data) => {
-        setOrderDetails(data);
-    }
-
-    useFocusEffect(
-        React.useCallback(() => {
-            setOrderData(route.params.orderDetailsData);
-        }, [])
-    );
-
-    useEffect(() => {
-        getRetailerDetails();
+  useFocusEffect(
+    React.useCallback(() => {
+      setOrderData(route.params.orderDetailsData);
     }, [])
+  );
 
-    const getRetailerDetails = () => {
-        const data = {
-            method: commonApi.getRetailerDetails.method,
-            url: commonApi.getRetailerDetails.url,
-            header: commonApi.getRetailerDetails.header
-        }
-        AuthenticatedGetRequest(data).then((res) => {
-            if (res.data) {
-                setRetailerDetails(res.data)
-            }
-        })
+  useEffect(() => {
+    getRetailerDetails();
+  }, []);
 
+  const getRetailerDetails = () => {
+    const data = {
+      method: commonApi.getRetailerDetails.method,
+      url: commonApi.getRetailerDetails.url,
+      header: commonApi.getRetailerDetails.header,
+    };
+    AuthenticatedGetRequest(data).then((res) => {
+      if (res.data) {
+        setRetailerDetails(res.data);
+      }
+    });
+  };
+
+  const downloadInvoice = (invoiceUrl) => {
+    if (invoiceUrl) {
+      WebBrowser.openBrowserAsync(invoiceUrl);
+    } else {
+      Alert.alert("Invoice not available. Order not approved yet.");
     }
+  };
 
-    const downloadInvoice = (invoiceUrl) => {
-        if (invoiceUrl) {
-            WebBrowser.openBrowserAsync(invoiceUrl);
-        } else {
-            Alert.alert("Invoice not available. Order not approved yet.")
-        }
-    }
-
-    return (
-        <ScrollView showsVerticalScrollIndicator={false} style={{flex: 1}}>
-            <View style={{paddingHorizontal: 12}}>
-                <SecondaryHeader title={"Order Details"}/>
-                <View style={styles.container}>
-                    <View style={commonStyles.rowSpaceBetween}>
-                        <View>
-                            <View style={commonStyles.rowAlignCenter}>
-                                <Text style={texts.darkGreyTextBold12}>
-                                    Order Id:
-                                </Text>
-                                <Text style={texts.primaryTextBold12}>
-                                    {orderDetails.id}
-                                </Text>
-                            </View>
-                            <View>
-                                <Text style={[texts.darkGreyTextBold14]}>
-                                    {retailerDetails.name}
-                                </Text>
-                            </View>
-                        </View>
-                    </View>
-                    <View
-                        style={[commonStyles.rowAlignCenter, styles.borderBottom, {paddingBottom: 10}]}>
-                        <Icon name="map-pin" size={18} color={colors.primary_color}/>
-                        <Text style={[texts.greyNormal12, {marginLeft: 10}]}>
-                            {retailerDetails.address_str}
-                        </Text>
-                    </View>
-                    <View>
-                        <View style={[styles.productHeader, commonStyles.rowSpaceBetween]}>
-                            <Text style={texts.blueBoldl14}>
-                                Ordered Items
-                            </Text>
-                            <Text style={texts.blueBoldl14}>
-                                Quantity
-                            </Text>
-                        </View>
-                        {orderDetails.product_list.map((item, index) => {
-                            let name = item.name;
-                            if (item.unit_label !== item.level_0_label) {
-                                name = item.name + " - " + item.unit_label
-                            }
-                            return (<View key={item.name + '' + index}
-                                          style={[styles.productListItem, styles.borderBottom]}>
-                                <View style={{width:"75%"}}>
-                                    <Text style={texts.darkGreyTextBold12}>
-                                        {name}
-                                    </Text>
-                                    <View style={[commonStyles.rowAlignCenter]}>
-                                        <Text style={texts.greyNormal12}>
-                                            MRR: {item.mrp}
-                                        </Text>
-                                        <Text style={[texts.greyNormal12, {marginLeft: 20}]}>
-                                            Rate: {parseFloat(item.rate * item.lot_quantity).toFixed(2)}
-                                        </Text>
-                                    </View>
-                                </View>
-                                <View style={{alignItems: "flex-end"}}>
-                                    <Text style={texts.darkGreyTextBold12}>
-                                        {item.value}
-                                    </Text>
-                                    {item.unit_label !== item.level_0_label ?
-                                        <Text style={texts.greyTextBold12}>
-                                            {"(" + item.value * item.lot_quantity + " " + item.level_0_label}{item.unit_label !== item.level_0_label || item.value > 1 ? "s" : ""}{")"}
-                                        </Text> : null}
-                                </View>
-                            </View>)
-                        })}
-                    </View>
-                    <View style={[styles.borderBottom, styles.orderSummary]}>
-                        <View style={styles.flexRow}>
-                            <Text style={texts.greyTextBold12}>
-                                Total Items:
-                            </Text>
-                            <Text style={texts.greyTextBold12}>
-                                {" " + orderDetails.revised_count}
-                            </Text>
-                        </View>
-                        <View style={styles.flexRow}>
-                            <Text style={texts.greyTextBold12}>
-                                {"Order Value: "}
-                            </Text>
-                            <Text style={texts.primaryTextBold12}>
-                                Rs {parseFloat(orderDetails.revised_value).toFixed(2)}
-                            </Text>
-                        </View>
-                    </View>
-                    {orderDetails.status === "delivered" ?
-                        <View style={{flexDirection: "row", justifyContent: "flex-end", marginTop: 10}}>
-                            <BorderButtonSmallBlue ctaFunction={() => {
-                                downloadInvoice(orderDetails.invoice)
-                            }} text={"Invoice"}/>
-                        </View> : null}
-                </View>
+  return (
+    <ScrollView showsVerticalScrollIndicator={false} style={{ flex: 1 }}>
+      <View style={{ paddingHorizontal: 12 }}>
+        <SecondaryHeader title={"Order Details"} />
+        <View style={styles.container}>
+          <View style={commonStyles.rowSpaceBetween}>
+            <View>
+              <View style={commonStyles.rowAlignCenter}>
+                <Text style={texts.darkGreyTextBold12}>Order Id: </Text>
+                <Text style={texts.primaryTextBold12}>{orderDetails.id}</Text>
+              </View>
+              <View>
+                <Text style={[texts.darkGreyTextBold14]}>
+                  Distributor Name: {orderDetails.distributor_name}
+                </Text>
+              </View>
+              <View>
+                <Text style={[texts.darkGreyTextBold14]}>
+                  Retailer Name: {retailerDetails.name}
+                </Text>
+              </View>
             </View>
-        </ScrollView>
-    )
+          </View>
+          <View
+            style={[
+              commonStyles.rowAlignCenter,
+              styles.borderBottom,
+              { paddingBottom: 10 },
+            ]}
+          >
+            <Icon name="map-pin" size={18} color={colors.primary_color} />
+            <Text style={[texts.greyNormal12, { marginLeft: 10 }]}>
+              {retailerDetails.address_str}
+            </Text>
+          </View>
+          <View>
+            <View style={[styles.productHeader, commonStyles.rowSpaceBetween]}>
+              <Text style={texts.blueBoldl14}>Ordered Items</Text>
+              <Text style={texts.blueBoldl14}>Quantity</Text>
+            </View>
+            {orderDetails.product_list.map((item, index) => {
+              let name = item.name;
+              if (item.unit_label !== item.level_0_label) {
+                name = item.name + " - " + item.unit_label;
+              }
+              return (
+                <View
+                  key={item.name + "" + index}
+                  style={[styles.productListItem, styles.borderBottom]}
+                >
+                  <View style={{ width: "75%" }}>
+                    <Text style={texts.darkGreyTextBold12}>{name}</Text>
+                    <View style={[commonStyles.rowAlignCenter]}>
+                      <Text style={texts.greyNormal12}>MRR: {item.mrp}</Text>
+                      <Text style={[texts.greyNormal12, { marginLeft: 20 }]}>
+                        Rate:{" "}
+                        {parseFloat(item.rate * item.lot_quantity).toFixed(2)}
+                      </Text>
+                    </View>
+                  </View>
+                  <View style={{ alignItems: "flex-end" }}>
+                    <Text style={texts.darkGreyTextBold12}>{item.value}</Text>
+                    {item.unit_label !== item.level_0_label ? (
+                      <Text style={texts.greyTextBold12}>
+                        {"(" +
+                          item.value * item.lot_quantity +
+                          " " +
+                          item.level_0_label}
+                        {item.unit_label !== item.level_0_label ||
+                        item.value > 1
+                          ? "s"
+                          : ""}
+                        {")"}
+                      </Text>
+                    ) : null}
+                  </View>
+                </View>
+              );
+            })}
+          </View>
+          <View style={[styles.borderBottom, styles.orderSummary]}>
+            <View style={styles.flexRow}>
+              <Text style={texts.greyTextBold12}>Total Items:</Text>
+              <Text style={texts.greyTextBold12}>
+                {" " + orderDetails.revised_count}
+              </Text>
+            </View>
+            <View style={styles.flexRow}>
+              <Text style={texts.greyTextBold12}>{"Order Value: "}</Text>
+              <Text style={texts.primaryTextBold12}>
+                Rs {parseFloat(orderDetails.revised_value).toFixed(2)}
+              </Text>
+            </View>
+          </View>
+          {orderDetails.status === "delivered" ? (
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "flex-end",
+                marginTop: 10,
+              }}
+            >
+              <BorderButtonSmallBlue
+                ctaFunction={() => {
+                  downloadInvoice(orderDetails.invoice);
+                }}
+                text={"Invoice"}
+              />
+            </View>
+          ) : null}
+        </View>
+      </View>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    headerContainer: {
-        flexDirection: 'row',
-        alignItems: "center",
-        marginTop: 20
-    },
-    container: {
-        marginTop: 20,
-        padding: 10,
-        borderRadius: 5,
-        borderColor: colors.grey,
-        backgroundColor: '#ffffff',
-        elevation: 2,
-        marginBottom: 20
-    },
-    retailerDiv: {
-        width: '100%',
-        flexDirection: "row",
-        justifyContent: 'space-between',
-        alignItems: "center",
-        paddingVertical: 10
-    },
-    callIconDiv: {
-        width: 30,
-        height: 30,
-        borderRadius: 5,
-        borderColor: colors.primary_color,
-        borderWidth: 1.5,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    callIcon: {
-        width: 15,
-        height: 15
-    },
-    productHeader: {
-        flexDirection: 'row',
-        justifyContent: "space-between",
-        paddingTop: 10
-    },
-    productListItem: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '100%',
-        paddingVertical: 6,
-        alignItems: 'center'
-    },
-    borderBottom: {
-        borderBottomColor: colors.grey,
-        borderBottomWidth: 1
-    },
-    orderSummary: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        paddingTop: 10,
-        paddingBottom: 10
-    },
-    flexRow: {
-        flexDirection: 'row',
-        alignItems: "center"
-    }
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  container: {
+    marginTop: 20,
+    padding: 10,
+    borderRadius: 5,
+    borderColor: colors.grey,
+    backgroundColor: "#ffffff",
+    elevation: 2,
+    marginBottom: 20,
+  },
+  retailerDiv: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  callIconDiv: {
+    width: 30,
+    height: 30,
+    borderRadius: 5,
+    borderColor: colors.primary_color,
+    borderWidth: 1.5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  callIcon: {
+    width: 15,
+    height: 15,
+  },
+  productHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingTop: 10,
+  },
+  productListItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingVertical: 6,
+    alignItems: "center",
+  },
+  borderBottom: {
+    borderBottomColor: colors.grey,
+    borderBottomWidth: 1,
+  },
+  orderSummary: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  flexRow: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
 });
