8234831b2eef31b4780b67fbecdee6018fbf3919
vishnu <vishnu@neopay.club>
2022-07-29 19:05:25 +0530

    retailer sentry add

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import store from "./store/store";
-import {Provider} from 'react-redux';
-import useCachedResources from './hooks/useCachedResources';
+import { Provider } from "react-redux";
+import useCachedResources from "./hooks/useCachedResources";
 import Routes from "./navigation/Routes";
 import colors from "./assets/colors/colors";
-import {StatusBar} from "expo-status-bar";
-import {SafeAreaView} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native";
+import * as Sentry from "@sentry/react-native";
 
+Sentry.init({
+  dsn: "https://75d529fb9c074630a7497937f0250abc@o1339126.ingest.sentry.io/6611131",
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
+  tracesSampleRate: 1.0,
+  enableInExpoDevelopment: true,
+  enableAutoPerformanceTracking: true,
+  enableOutOfMemoryTracking: true,
+  debug: true,
+});
 export default function App() {
   const isLoadingComplete = useCachedResources();
 
@@ -14,13 +25,15 @@ export default function App() {
     return null;
   } else {
     return (
-
-        <Provider store={store}>
-          <StatusBar barStyle="light-content" backgroundColor={colors.primaryThemeColor}/>
-          <SafeAreaView style={{flex: 1, marginTop: 30}}>
-            <Routes/>
-          </SafeAreaView>
-        </Provider>
-    )
+      <Provider store={store}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={colors.primaryThemeColor}
+        />
+        <SafeAreaView style={{ flex: 1, marginTop: 30 }}>
+          <Routes />
+        </SafeAreaView>
+      </Provider>
+    );
   }
 }
